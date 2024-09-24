a3151c8059e592ed7521f7c98e48522c723b24b0
ravan023 <shivamshukla424242@gmail.com>
2024-09-24 16:10:46 +0530

    url  update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://ravan-chat-1.onrender.com/", {
+      const socket = io("https://shivam-5j9r.onrender.com", {
         query: {
           userId: authUser._id,
         },
